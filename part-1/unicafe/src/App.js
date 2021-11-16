0ccb6657ff49241b7cea957267cfc76fc4166cad
Gerog13 <gerog13@hotmail.com>
2021-11-15 21:39:20 -0300

    unicafe exercise first commit, exercise started

---
+++ part-1/unicafe/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const DisplayValues = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <header>
+        <h2>Give Feedback</h2>
+      </header>
+      <section>
+        <Button handleClick={() => setGood(good + 1)} text="Good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      </section>
+      <DisplayValues good={good} neutral={neutral} bad={bad} />
+    </>
+  );
+};
+
+export default App;
