4efb7e00d41bebcb9bc77e539e3872690acb032a
Gerog13 <gerog13@hotmail.com>
2021-11-16 20:13:12 -0300

    anecdotes step2, vote system added

--- part-1/anecdotes/src/App.js
+++ part-1/anecdotes/src/App.js
@@ -1,28 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
-   
-  const [selected, setSelected] = useState(0)
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ];
 
-  console.log(selected);
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+  });
+
+  const voteAnecdoteHandler = (idAnecdote) => {
+    setPoints({...points, [idAnecdote]: points[idAnecdote] + 1 });
+  };
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
-
-      <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} >
+      <p>has {points[selected]} votes</p>
+      <button onClick={() => voteAnecdoteHandler(selected)}>Vote</button>
+      <button
+        onClick={() =>
+          setSelected(Math.floor(Math.random() * anecdotes.length))
+        }
+      >
         Next anecdote
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
