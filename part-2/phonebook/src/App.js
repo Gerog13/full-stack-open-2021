f0aa5daac9f1970ae565471f465d539464fd33a1
Gerog13 <gerog13@hotmail.com>
2021-11-23 20:20:45 -0300

    phonebook step4, persons name filter implemented

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", phone: 21314 }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,14 +24,21 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons([...persons, { name: newName, phone: phoneNumber }]);
+    setPersons([...persons, { name: newName, number: phoneNumber, id: Math.random() }]);
     setNewName("");
     setPhoneNumber('');
   };
 
+
+  const personsToShow = filter.trim().length === 0 ? persons : persons.filter((person) => person.name.toLowerCase().includes(filter));
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)}/>
+      </div>
+      <h3>Add a new</h3>
       <form onSubmit={handleSubmit}>
         <div>
           name: 
@@ -48,9 +61,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons?.map((person) => (
-        <p key={person.phone}>{person.name} {person.phone}</p>
+      {personsToShow?.map((person) => (
+        <p key={person.number}>{person.name} {person.number}</p>
       ))}
+      {/* {persons?.map((person) => (
+        <p key={person.number}>{person.name} {person.number}</p>
+      ))} */}
     </div>
   );
 };
