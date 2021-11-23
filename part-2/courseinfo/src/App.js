40793d430544ccd72b3aa1999dfee64a16cef19a
Gerog13 <gerog13@hotmail.com>
2021-11-23 11:00:33 -0300

    courseinfo separate module Course implemented

--- part-2/courseinfo/src/App.js
+++ part-2/courseinfo/src/App.js
@@ -1,45 +1,5 @@
 import React from "react";
-
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
-};
-
-const Total = ({ course }) => {
-  const sum = course?.parts?.reduce((sum, part) => sum + part.exercises, 0);
-  return (
-    <p>
-      <strong>Total of {sum} exercises</strong>
-    </p>
-  );
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course?.parts?.map((part) => {
-        return <Part part={part} key={part.id} />;
-      })}
-    </div>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </>
-  );
-};
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
