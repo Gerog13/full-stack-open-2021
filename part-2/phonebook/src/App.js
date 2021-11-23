de1a1d465008add978c00d28664cfe6924193965
Gerog13 <gerog13@hotmail.com>
2021-11-23 12:28:02 -0300

    phonebook exercise started, add name to list feature added

---
+++ part-2/phonebook/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newName.trim().length === 0) {
+      alert("Invalid name");
+      return;
+    }
+    setPersons([...persons, { name: newName }]);
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons?.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
